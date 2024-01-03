47f0dc4d8240ce83ca0596d8037e9b3e2115250e
agus1-acosta <aguz.271976@gmail.com>
2024-01-03 17:28:36 -0300

    ajuste los detalles que me faltaban

--- src/app.js
+++ src/app.js
@@ -16,3 +16,14 @@ console.log("se prendio en el puerto" + PORT);
 app.get("/", (req,res) => {
 res.sendFile(path.join(__dirname,"views/home.html"));
 });
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get(('/register'), (req, res) => 
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+);
+
+app.get(('/login'), (req, res) => 
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+);
